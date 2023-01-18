c2929a258af62effa357a09f9b4d04dc70ddea49
Karan Bhatia <karan.bhatia@gmail.com>
2023-01-18 10:21:44 -0500

    tested with flask-dance (upstream)

--- dexcom.py
+++ dexcom.py
@@ -6,14 +6,18 @@ app = Flask(__name__)
 app.secret_key = os.environ.get("FLASK_SECRET_KEY", "supersekrit")
 app.config["DEXCOM_OAUTH_CLIENT_ID"] = os.environ.get("DEXCOM_OAUTH_CLIENT_ID")
 app.config["DEXCOM_OAUTH_CLIENT_SECRET"] = os.environ.get("DEXCOM_OAUTH_CLIENT_SECRET")
-github_bp = make_github_blueprint()
-app.register_blueprint(github_bp, url_prefix="/login")
-
+dexcom_bp = make_dexcom_blueprint(
+    scope="offline_access", 
+    redirect_url="http://localhost:5000/login/dexcom/authorized")
+app.register_blueprint(dexcom_bp, url_prefix="/login")
 
 @app.route("/")
 def index():
-    if not github.authorized:
+
+    if not dexcom.authorized:
         return redirect(url_for("dexcom.login"))
-    resp = github.get("/user")
+    
+    resp = dexcom.get("/v2/users/self/dataRange")
     assert resp.ok
-    return "You are @{login} on Dexcom".format(login=resp.json()["login"])
+    return "Datarange = @{login} from Dexcom".format(login=resp.json())
+
